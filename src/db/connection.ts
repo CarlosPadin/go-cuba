19b525b4e6d29e3c356fa749d70e1aa329bd44ca
Padin <padin790@gmail.com>
2025-09-29 15:05:46 -0400

    Added function to extract one car by its id

--- src/db/connection.ts
+++ src/db/connection.ts
@@ -4,11 +4,12 @@ import { parseCar } from './parse';
 
 const db = sql('yava.db');
 
+// Function to get all cars from the database
 export const getCars = () => {
-
   return db.prepare('SELECT * FROM cars').all();
 };
 
+// Function to get cars by their type, with an optional limit
 export const getCarsByType = ({carType, limit}: {carType: string, limit?: number}): Car[] => {
   let query = 'SELECT * FROM cars WHERE carType = ?';
   if (limit) {
@@ -19,3 +20,10 @@ export const getCarsByType = ({carType, limit}: {carType: string, limit?: number
   const data = db.prepare(query).all(carType) as Car[]; 
   return data.map(parseCar);
 };
+
+// Function to get one car by its ID
+export const getCarById = (id: string): Car => {
+  const car = db.prepare('SELECT * FROM cars WHERE id = ?').get(id) as Car;
+  if (!car) throw new Error('Car not found');
+  return parseCar(car);
+}
