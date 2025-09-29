d8e17fab53a2c7a05ef7089e2ad91a4a28f1ee9b
Padin <padin790@gmail.com>
2025-09-29 15:01:39 -0400

    Small fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import styles from "./page.module.css";
 import { Box } from "@mui/material";
 import ExploreCars from "../components/sections/explore/ExploreCars";
-import SearchBarHolder from "../components/ui/search-bar/searchBarHolder";
 import Banner from "../components/sections/banner";
 
 
@@ -10,7 +9,6 @@ export default function Home() {
   return (
     <div className={styles.page}>
       <main>
-        {/* <SearchBarHolder /> */}
         <Banner /> 
         <Box
           display="flex"
