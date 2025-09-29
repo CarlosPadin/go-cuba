e7b84896070886a537f1deaea8456fb2479346d6
Padin <padin790@gmail.com>
2025-09-29 12:09:18 -0400

    Fixed small error on link

--- src/components/sections/explore/cars/CarsPreviewGrid.tsx
+++ src/components/sections/explore/cars/CarsPreviewGrid.tsx
@@ -35,7 +35,7 @@ const CarsPreviewGrid: FC<Props> = ({ carType }) => {
         >
           {t(carType)}
         </Typography>
-        <Link href={"/cars/" + carType}>
+        <Link href={carType}>
           <Tooltip title={t("seeMore")} placement="top">
             <ArrowForwardIos
               sx={{
